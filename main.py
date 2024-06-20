82b0a39b6078eb9648ba28a87499bad7f8ea7784
kevinkeyssx <keyss1700@gmail.com>
2024-06-19 21:50:26 -0400

    chore: Add localhost to CORS origins in main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "https://profile-kevinkeyssx.vercel.app",
         "https://www.kevinkeyssx.com",
         "https://www.kevinkeyssx.dev",
+        "http://localhost:3000",
     ],
     allow_credentials   = True,
     allow_methods       = ["*"],
